324e17e040e575517c7c03254503270710823d22
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2023-08-11 13:43:21 +0600

    No expand icon shown if there is no owner

--- src/components/entity-table/ExpandingCellComponent.tsx
+++ src/components/entity-table/ExpandingCellComponent.tsx
@@ -2,6 +2,7 @@ import { IconButton, Table } from 'rsuite'
 import { Entity } from '../../types/entity-types'
 import CollaspedOutlineIcon from '@rsuite/icons/CollaspedOutline';
 import ExpandOutlineIcon from '@rsuite/icons/ExpandOutline';
+import { EntityControlUtils } from '../../utils/entity-utils/entity-control-utils';
 
 type ExpandingCellComponentProps = {
     rowData: Entity,
@@ -13,22 +14,30 @@ type ExpandingCellComponentProps = {
 
 export default function ExpandingCellComponent({ rowData, dataKey, expandedRowKeys, handleExpand, ...props }: ExpandingCellComponentProps) {
 
+    const noOfOwners = EntityControlUtils.getOwnedOwnerships(rowData).length;
+
     return (
         <>
             <Table.Cell style={{ padding: 5 }} {...props} >
-                <IconButton
-                    appearance="subtle"
-                    onClick={() => {
-                        handleExpand(rowData);
-                    }}
-                    icon={
-                        expandedRowKeys.some(key => key === rowData[dataKey as (keyof typeof rowData)]) ? (
-                            <CollaspedOutlineIcon />
-                        ) : (
-                            <ExpandOutlineIcon />
-                        )
-                    }
-                />
+                {
+                    (noOfOwners > 0) ?
+                        <IconButton
+                            appearance="subtle"
+                            onClick={() => {
+                                handleExpand(rowData);
+                            }}
+                            icon={
+                                expandedRowKeys.some(key => key === rowData[dataKey as (keyof typeof rowData)]) ? (
+                                    <CollaspedOutlineIcon />
+                                ) : (
+                                    <ExpandOutlineIcon />
+                                )
+                            }
+                        /> :
+                        null
+                }
+
+
             </Table.Cell>
         </>
     )
